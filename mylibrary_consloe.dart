1c2fcacc34f88234eed1d45efa1ad527a92c5979
HeilanElf <3263718994@qq.com>
2023-05-22 04:04:45 +0000

    add

---
+++ mylibrary_consloe.dart
@@ -0,0 +1,4 @@
+void main(){
+  var tea="wulong tea";
+  print("I like drink $tea");
+}
---
+++ mylibrary_string.dart
@@ -0,0 +1,35 @@
+void main(){
+  // Check whether a string contains another string.
+  assert('Never odd or even'.contains('odd'));
+
+  // Does a string start with another string?
+  assert('Never odd or even'.startsWith('Never'));
+
+  // Does a string end with another string?
+  assert('Never odd or even'.endsWith('even'));
+
+  // Find the location of a string inside a string.
+  assert('Never odd or even'.indexOf('odd') == 6);
+
+  // Grab a substring.
+  assert('Never odd or even'.substring(6, 9) == 'odd');
+
+  // Split a string using a string pattern.
+  var parts = 'progressive web apps'.split(' ');
+  assert(parts.length == 3);
+  assert(parts[0] == 'progressive');
+
+  // Get a UTF-16 code unit (as a string) by index.
+  assert('Never odd or even'[0] == 'N');
+
+  // Use split() with an empty string parameter to get
+  // a list of all characters (as Strings); good for
+  // iterating.
+  for (final char in 'hello'.split('')) {
+    print(char);
+  }
+
+  // Get all the UTF-16 code units in the string.
+  var codeUnitList = 'Never odd or even'.codeUnits.toList();
+  assert(codeUnitList[0] == 78);
+}
